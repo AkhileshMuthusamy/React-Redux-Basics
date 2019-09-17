7f217c763d8072ecc7b8d9b91127f1d7573c58f4
Akhilesh <AkhileshM@outlook.in>
2019-09-17 19:25:18 +0530

    Updated code to handle logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const logger = createLogger();
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(logger)));
 
 export default store;
