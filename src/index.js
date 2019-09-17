dad68f61f3dfd208d04dcfcc39a01216956f14e4
Akhilesh <AkhileshM@outlook.in>
2019-09-17 19:06:47 +0530

    Naively re-rendered react component

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ import store from './store';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import { getReadableStories } from './selectors/story';
 
-ReactDOM.render(
-  <App stories={getReadableStories(store.getState())} onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })} />,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
