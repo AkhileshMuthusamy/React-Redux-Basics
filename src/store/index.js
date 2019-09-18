79eb6119e4e584f9486de19af5de2d13f50971d9
Akhilesh <AkhileshM@outlook.in>
2019-09-18 18:09:44 +0530

    Added saga middleware to redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const logger = createLogger();
+const saga = createSagaMiddleware();
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(saga, logger)));
+
+saga.run(rootSaga);
 
 export default store;
